0897118adb5d65466fc21b74a9d1b266ea7bc1a5
Alexander Popiak <alexander.popiak@gmail.com>
2018-12-07 14:05:15 +0100

    add some proptests

---
+++ tests/proptests.rs
@@ -0,0 +1,63 @@
+#[macro_use]
+extern crate proptest;
+use proptest::prelude::*;
+
+use proptest::collection::vec;
+
+use hollow_heap::HollowHeap;
+
+fn take<T>(mut vec: Vec<T>, index: usize) -> Option<T> {
+    if vec.get(index).is_none() {
+        None
+    } else {
+        Some(vec.swap_remove(index))
+    }
+}
+
+proptest! {
+
+    #[test]
+    fn doesnt_crash(num in 0..100000) {
+        let mut heap = HollowHeap::new();
+        heap.push(num);
+        assert!(heap.pop() == Some(num));
+        assert!(heap.pop() == None);
+    }
+
+    #[test]
+    fn repeated_pop_returns_sorted_vec(vector in vec(u32::arbitrary(), 0..1000)) {
+        println!("{:?}", vector);
+        let mut heap = HollowHeap::new();
+        for num in vector.iter() {
+            heap.push(num);
+        }
+
+        let mut sorted = vector.clone();
+        sorted.sort_by(|a, b| b.cmp(a));
+        for num in sorted.iter() {
+            prop_assert_eq!(heap.pop(), Some(num));
+        }
+    }
+
+    #[test]
+    fn doesnt_crash_with_delete_and_increase_key(vector in vec(u32::arbitrary(), 2..1000)) {
+        println!("{:?}", vector);
+        let mut heap = HollowHeap::new();
+        let mut index = None;
+        let mut second_index = None;
+        for num in vector.iter() {
+            if index.is_none() {
+                index = Some(heap.push(*num));
+            } else if second_index.is_none() {
+                second_index = Some(heap.push(*num));
+            }
+        }
+
+        let index = index.unwrap();
+        let second_index = second_index.unwrap();
+
+        let value = *heap.peek().unwrap();
+        heap.increase_key(index, value + 1);
+        heap.delete(second_index);
+    }
+}
